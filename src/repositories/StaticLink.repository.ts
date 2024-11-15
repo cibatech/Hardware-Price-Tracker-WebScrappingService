7da8e4f0f6a9e81381791e95fa4757b90f9b5190
ciringa <thierrird957468@gmail.com>
2024-11-15 13:13:44 -0300

    scrap:pichau link list

--- src/repositories/StaticLink.repository.ts
+++ src/repositories/StaticLink.repository.ts
@@ -2,5 +2,5 @@ import { Prisma, StaticLink } from "../../prisma/indev-output";
 
 export interface StaticLinkRepository {
     create(data:Prisma.StaticLinkCreateInput):Promise<StaticLink>
-    delete(id:string):Promise<StaticLink|null>
+    delete(id:number):Promise<StaticLink|null>
 }
--- src/utils/scrap/PichauGetCategoryLinkList.ts
+++ src/utils/scrap/PichauGetCategoryLinkList.ts
@@ -2,6 +2,10 @@ import puppeteer from "puppeteer"
 import { PichauLinkCollection } from "../../collections/pichauLinkCollection";
 import { waitForDebugger } from "inspector";
 
+interface linkList{
+    Link:string,
+}
+
 (async()=>{
     const browser = await puppeteer.launch({
         headless:false
@@ -16,14 +20,17 @@ import { waitForDebugger } from "inspector";
     })
 
     await page.click("div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-md-4.MuiGrid-grid-xl-3 > button.MuiButtonBase-root[aria-label='menu']")
-
+    await page.waitForSelector("div.MuiPaper-root.MuiDrawer-paper.MuiDrawer-paperAnchorLeft.MuiPaper-elevation16")
     const ps = await page.evaluate(()=>{
-        const DOMCore = document.querySelector(".jss370") as HTMLDivElement;
+        const DOMCore = document.querySelector("div.MuiPaper-root.MuiDrawer-paper.MuiDrawer-paperAnchorLeft.MuiPaper-elevation16") as HTMLDivElement;
         const LiList = DOMCore.querySelectorAll("li" ) as NodeListOf<HTMLLIElement>
-        var resList:string[] = []
+        var resList:linkList[] = []
 
         LiList.forEach(Element=>{
-            resList.push(Element.innerHTML)
+            const searchForA = Element.querySelector("a") as HTMLAnchorElement
+            resList.push({
+                Link:searchForA.href
+            })
         })
 
         return resList
